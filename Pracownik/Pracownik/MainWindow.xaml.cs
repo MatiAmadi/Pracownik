0fc60a884a240e9280d897e043ae8ab8ec0fe126
Mati R <108244003+MatiAmadi@users.noreply.github.com>
2025-09-11 09:23:41 +0200

    Zastosowanie

--- Pracownik/Pracownik/MainWindow.xaml.cs
+++ Pracownik/Pracownik/MainWindow.xaml.cs
@@ -11,14 +11,11 @@ using System.Windows.Shapes;
 
 namespace Pracownik
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            
+            InitializeComponent();
         }
     }
 }
